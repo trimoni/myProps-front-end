6f5b0f2974823b001a7832e4e1c1a18c9606db52
Tal <trimoni8@gmail.com>
2022-10-24 14:12:08 -0400

    bedroom listing

--- src/pages/AddListing/AddListing.jsx
+++ src/pages/AddListing/AddListing.jsx
@@ -10,11 +10,56 @@ const AddListing = (props) => {
     details: '',
   })
 
-  
+  const handleChange = ({ target }) => {
+    setFormData({...formData, [target.name]: target.value})
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    props.handleAddListing(formData)
+  }
+
+
   return (
-    <>
-    
-    </>
+    <main>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="address">Address</label>
+        <input
+          required
+          type="text"
+          name="address"
+          id="address"
+          value={formData.address}
+          placeholder="Address"
+          onChange={handleChange}
+        />
+        <label htmlFor="bedroom">Bedroom</label>
+				<textarea
+          required
+          name="bedroom"
+          id="text-input"
+          value={formData.text}
+          placeholder="Text"
+          onChange={handleChange}
+        />
+        <label htmlFor="category-input">Category</label>
+        <select
+          required
+          name="category"
+          id="category-input"
+          value={form.category}
+          onChange={handleChange}
+        >
+          <option value="News">News</option>
+          <option value="Games">Games</option>
+          <option value="Music">Music</option>
+          <option value="Movies">Movies</option>
+          <option value="Sports">Sports</option>
+          <option value="Television">Television</option>
+        </select>
+        <button type="submit">SUBMIT</button>
+      </form>
+    </main>
   );
 }
 
