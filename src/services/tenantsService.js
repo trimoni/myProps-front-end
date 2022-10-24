645153a5066444539d05a29e383ef392f7e22aaa
Tal <trimoni8@gmail.com>
2022-10-24 13:39:34 -0400

    tenant index

--- src/services/tenantsService.js
+++ src/services/tenantsService.js
@@ -1,3 +1,22 @@
 import * as tokenService from './tokenService'
 
 const BASE_URL = `${process.env.REACT_APP_BACK_END_SERVER_URL}/api/tenants`
+
+const index = async () => {
+  try {
+    const res = await fetch(BASE_URL, {
+      headers: { 'Authorization': `Bearer ${tokenService.getToken()}` },
+    })
+    return res.json()
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const show = async (id) => {
+  try {
+    const res = await fetch(``)
+  } catch (error) {
+    
+  }
+}
