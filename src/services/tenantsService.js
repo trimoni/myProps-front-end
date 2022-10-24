2a6ef22790f2f51032b73f8fb6e415b21f6111e4
Tal <trimoni8@gmail.com>
2022-10-24 13:41:09 -0400

    tenant show

--- src/services/tenantsService.js
+++ src/services/tenantsService.js
@@ -15,8 +15,16 @@ const index = async () => {
 
 const show = async (id) => {
   try {
-    const res = await fetch(``)
+    const res = await fetch(`${BASE_URL}/${id}`, {
+      headers: { 'Authorization': `Bearer ${tokenService.getToken()}` },
+    })
+    return res.json()
   } catch (error) {
-    
+    console.log(error)
   }
+}
+
+export {
+  index,
+  show,
 }
