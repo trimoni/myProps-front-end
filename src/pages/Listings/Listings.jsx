ad9e55048d6f387a902c82e2733c8c6c0de2916b
julianboyne <julianboyne11@gmail.com>
2022-10-24 22:40:22 -0400

    stub listings component

--- src/pages/Listings/Listings.jsx
+++ src/pages/Listings/Listings.jsx
@@ -1,14 +1,15 @@
 import "./Listings.css"
 
 
-const Listings = (props) => {
+const Listings = ({user , listing}) => {
   return ( 
     <div className="listing-container">
       <h1>Listings</h1>
-  
-      {props.listing.map(l => (
-        <li>{l.address}</li>
-      ))}
+      {/* map through the listings to get the user's listings */}
+      {/* if user have a listing, then display the listings */}
+      {/* if user doesn't have a listing, then display "No listing" */}
+
+
     
     </div>
   );
