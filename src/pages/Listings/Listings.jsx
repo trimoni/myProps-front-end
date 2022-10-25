aac6a49ddc00577e884a4c6730f1dff5d430a570
Tal <trimoni8@gmail.com>
2022-10-25 10:36:50 -0400

    listing page

--- src/pages/Listings/Listings.jsx
+++ src/pages/Listings/Listings.jsx
@@ -1,12 +1,13 @@
+import ListingCard from "../../components/ListingCard/ListingCard";
 import "./Listings.css"
 
 
-const Listings = ({user , listings}) => {
+const Listings = ({ listings }) => {
   return ( 
     <div className="listing-container">
       <h1>Listings</h1>
-      {listings.map(l => (
-        <li>{l.address}</li>
+      {listings.map((l) => (
+        <ListingCard listing={l} key={l._id}/>
       ))}
       {/* map through the listings to get the user's listings */}
       {/* if user have a listing, then display the listings */}
