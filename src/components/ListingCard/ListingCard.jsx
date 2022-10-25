dfad6a3cd8cabe114b9143187541358f4bd19f11
Tal <trimoni8@gmail.com>
2022-10-25 10:45:25 -0400

    listing card

--- src/components/ListingCard/ListingCard.jsx
+++ src/components/ListingCard/ListingCard.jsx
@@ -1,9 +1,18 @@
+import { Link } from "react-router-dom"
 
-const ListingCard = () => {
+const ListingCard = ({ listing }) => {
   return ( 
-    <>
-    Card details
-    </>
+    <Link to={`/listing/${listing._id}`}>
+      <article>
+        <header>
+          <span>
+            <img src={listing.photo} alt="property" />
+            <h1>{listing.address}</h1>
+            <h1>{listing.price}</h1>
+          </span>
+        </header>
+      </article>
+      </Link>
   );
 }
 
