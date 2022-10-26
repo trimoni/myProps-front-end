2fb7658ee0b5c58f6026d244b382b3c88b0d1a96
Chris Mems <christopher.mems@.com>
2022-10-26 12:29:24 -0500

    create tenant card

--- src/components/TenantCard/TenantCard.jsx
+++ src/components/TenantCard/TenantCard.jsx
@@ -1,10 +1,13 @@
-import { Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import styles from './TenantCard.module.css'
+
 
 const TenantCard = ({ tenant, handleDeleteTenant }) => {
   return ( 
     <>
-      <button onClick={() => handleDeleteTenant(tenant._id)}>X</button>
+    <button onClick={() => handleDeleteTenant(tenant._id)}>X</button>
     <Link to={`/tenants/${tenant._id}/edit`} state={tenant}>
+      <article className={styles.container}>
       <p>
         Name: {tenant.name}
       </p>
@@ -17,6 +20,7 @@ const TenantCard = ({ tenant, handleDeleteTenant }) => {
       <p>
         Salary: ${tenant.salary} per year
       </p>
+      </article>
     </Link>
     </>
   );
