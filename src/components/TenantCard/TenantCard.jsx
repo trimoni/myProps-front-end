af4a0603ede374dc9a79dcf6c52cf0135e4de82f
Chris Mems <christopher.mems@.com>
2022-10-26 14:33:33 -0500

    add media to TenantList

--- src/components/TenantCard/TenantCard.jsx
+++ src/components/TenantCard/TenantCard.jsx
@@ -8,18 +8,18 @@ const TenantCard = ({ tenant, handleDeleteTenant }) => {
     <button onClick={() => handleDeleteTenant(tenant._id)}>X</button>
     <Link to={`/tenants/${tenant._id}/edit`} state={tenant}>
       <article className={styles.container}>
-      <p>
-        Name: {tenant.name}
-      </p>
-      <p>
-        Lease: {tenant.lease}
-      </p>
-      <p>
-        Job/Jobs: {tenant.jobs}
-      </p>
-      <p>
-        Salary: ${tenant.salary} per year
-      </p>
+        <p>
+          Name: {tenant.name}
+        </p>
+        <p>
+          Lease: {tenant.lease}
+        </p>
+        <p>
+          Job/Jobs: {tenant.jobs}
+        </p>
+        <p>
+          Salary: ${tenant.salary} per year
+        </p>
       </article>
     </Link>
     </>
--- src/pages/TenantList/TenantList.jsx
+++ src/pages/TenantList/TenantList.jsx
@@ -1,13 +1,16 @@
 import TenantCard from "../../components/TenantCard/TenantCard";
+import styles from './TenantList.css'
 
 
 const TenantList = ({ tenants, handleDeleteTenant }) => {
   return (
     <>
       <h2>List of all Tenants</h2>
-      {tenants.map(tenant =>
-          <TenantCard key={tenant._id} tenant={tenant} handleDeleteTenant={handleDeleteTenant}/>
-      )}
+      <main className={styles.container}>
+        {tenants.map(tenant =>
+            <TenantCard key={tenant._id} tenant={tenant} handleDeleteTenant={handleDeleteTenant}/>
+        )}
+      </main>
     </>
   );
 }
