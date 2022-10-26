8bf96d25f95c3645c69a9fbf3d868a7bf60a19ed
HiepTrinh <xmikecolx@gmail.com>
2022-10-26 09:07:34 -0500

    fix merge issues

--- src/components/ListingCard/ListingCard.jsx
+++ src/components/ListingCard/ListingCard.jsx
@@ -3,17 +3,22 @@ import './ListingCard.css'
 
 const ListingCard = ({ listing }) => {
   return (
-    <Link to={`/listing/${listing._id}/edit`} state={listing}>
-      <article>
-        <header>
-          <span>
-            <img src={listing.photo} alt="property" />
-            <h1>{listing.address}</h1>
-            <h1>{listing.price}</h1>
-          </span>
-        </header>
-      </article>
-    </Link>
+    <>
+      <Link to={`/listing/${listing._id}/edit`} state={listing}>
+        <article>
+          <header>
+            <span>
+              <img src={listing.photo} alt="property" />
+              <h1>{listing.address}</h1>
+              <h1>{listing.price}</h1>
+            </span>
+          </header>
+        </article>
+      </Link>
+      <Link
+        to='/listings/:id/workRequests'
+        state={listing}>Add Work request</Link>
+    </>
   );
 };
 
