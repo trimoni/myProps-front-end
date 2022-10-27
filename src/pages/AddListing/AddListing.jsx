29c32042e4ad6a5a6821959557ba6ba82832661c
Tal <trimoni8@gmail.com>
2022-10-27 17:45:57 -0400

    fix changes

--- src/pages/AddListing/AddListing.jsx
+++ src/pages/AddListing/AddListing.jsx
@@ -29,107 +29,109 @@ const AddListing = (props) => {
   };
 
   return (
-    <main className="container">
-      <form onSubmit={handleSubmit}>
+    <>
       <h1>Add Listing</h1>
-        <div>
-          <label htmlFor="address">Address</label>
-          <input
-            required
-            type="text"
-            name="address"
-            id="address"
-            value={form.address}
-            placeholder="Address"
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="photo">Upload Picture</label>
-          <input
-            multiple
-            type="file"
-            id="photo"
-            name="photo"
-            onChange={handleChangePhoto}
-          />
-        </div>
-        <div>
-          <label htmlFor="rent">Monthly Rent:</label>
-          <input
-            required
-            type="text"
-            name="rent"
-            id="rent"
-            value={form.rent}
-            placeholder="Rent"
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="bedroom">Bedroom</label>
-          <select
-            required
-            name="bedroom"
-            id="bedroom"
-            value={form.bedroom}
-            placeholder="Num"
-            onChange={handleChange}
-          >
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-            <option value="6+">6+</option>
-          </select>
-        </div>
-        <div>
-          <label htmlFor="bathroom">Bedroom</label>
-          <select
-            required
-            name="bathroom"
-            id="bathroom"
-            value={form.bathroom}
-            placeholder="Num"
-            onChange={handleChange}
-          >
-            <option value="1">1</option>
-            <option value="1.5">1.5</option>
-            <option value="2">2</option>
-            <option value="2.5">2.5</option>
-            <option value="3">3</option>
-            <option value="3+">3+</option>
-          </select>
-        </div>
-        <div>
-          <label htmlFor="pets">Pets:</label>
-          <input
-            required
-            type="text"
-            name="pets"
-            id="pets"
-            value={form.pets}
-            placeholder="Cat, Dog, etc..."
-            onChange={handleChange}
-          />
-        </div>
-        <label htmlFor="details">Details:</label>
-        <div>
-          <textarea
-            required
-            type="text"
-            name="details"
-            id="details"
-            value={form.details}
-            placeholder="Details"
-            onChange={handleChange}
-          />
-        </div>
-        <button type="submit">Add Listing</button>
-      </form>
-    </main>
+      <main className="container">
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="address">Address</label>
+            <input
+              required
+              type="text"
+              name="address"
+              id="address"
+              value={form.address}
+              placeholder="Address"
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="photo">Upload Picture</label>
+            <input
+              multiple
+              type="file"
+              id="photo"
+              name="photo"
+              onChange={handleChangePhoto}
+            />
+          </div>
+          <div>
+            <label htmlFor="rent">Monthly Rent:</label>
+            <input
+              required
+              type="text"
+              name="rent"
+              id="rent"
+              value={form.rent}
+              placeholder="Rent"
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="bedroom">Bedroom</label>
+            <select
+              required
+              name="bedroom"
+              id="bedroom"
+              value={form.bedroom}
+              placeholder="Num"
+              onChange={handleChange}
+            >
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="6+">6+</option>
+            </select>
+          </div>
+          <div>
+            <label htmlFor="bathroom">Bedroom</label>
+            <select
+              required
+              name="bathroom"
+              id="bathroom"
+              value={form.bathroom}
+              placeholder="Num"
+              onChange={handleChange}
+            >
+              <option value="1">1</option>
+              <option value="1.5">1.5</option>
+              <option value="2">2</option>
+              <option value="2.5">2.5</option>
+              <option value="3">3</option>
+              <option value="3+">3+</option>
+            </select>
+          </div>
+          <div>
+            <label htmlFor="pets">Pets:</label>
+            <input
+              required
+              type="text"
+              name="pets"
+              id="pets"
+              value={form.pets}
+              placeholder="Cat, Dog, etc..."
+              onChange={handleChange}
+            />
+          </div>
+          <label htmlFor="details">Details:</label>
+          <div>
+            <textarea
+              required
+              type="text"
+              name="details"
+              id="details"
+              value={form.details}
+              placeholder="Details"
+              onChange={handleChange}
+            />
+          </div>
+          <button type="submit">Add Listing</button>
+        </form>
+      </main>
+    </>
   );
 };
 
