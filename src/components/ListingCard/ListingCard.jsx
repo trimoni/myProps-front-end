047a7aabbf186663749cf553d46249114cd1a185
julianboyne <julianboyne11@gmail.com>
2022-10-27 13:08:20 -0400

    implement bootstrap card on listingCard

--- src/components/ListingCard/ListingCard.jsx
+++ src/components/ListingCard/ListingCard.jsx
@@ -1,33 +1,33 @@
 import { Link } from "react-router-dom";
 import "./ListingCard.css";
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup'
 
 const ListingCard = ({ listing }) => {
 
   return (
-    <>
       <Link to={`/listing/${listing._id}/edit`} state={listing}>
-        <article>
-          <header>
-            <span>
-              <div>
-                {listing.photo ? (
-                  <img src={listing.photo} alt="" height="200" width="200" />
-                ) : (
-                  <div>No Photos</div>
-                )}
-              </div>
-              <div>
-                <h1>{listing.address}</h1>
-                <h1>{listing.rent}</h1>
-              </div>
-            </span>
-          </header>
-        </article>
-      </Link>
-      <Link to="/listings/:id/workRequests" state={listing}>
-        Add Work request
-      </Link>
-    </>
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src={listing.photo} />
+          <Card.Body>
+            <Card.Title>{listing.address}</Card.Title>
+          </Card.Body>
+          <ListGroup className="list-group-flush">
+            <ListGroup.Item>Bed: {listing.bedroom} / Bath: {listing.bathroom}</ListGroup.Item>
+            <ListGroup.Item>Rent: ${listing.rent}</ListGroup.Item>
+          </ListGroup>
+          <Card.Text>
+            {}
+          </Card.Text>
+          <Card.Body>
+            <Link 
+              to="/listings/:id/workRequests" 
+              state={listing}>
+              Add Work request
+          </Link>
+        </Card.Body>
+      </Card>
+    </Link>
   );
 };
 
