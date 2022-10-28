39b662298388609ee8d543c393b117d484431847
julianboyne <julianboyne11@gmail.com>
2022-10-28 01:51:47 -0400

    work on listing card

--- src/components/ListingCard/ListingCard.jsx
+++ src/components/ListingCard/ListingCard.jsx
@@ -6,39 +6,45 @@ import ListGroup from "react-bootstrap/ListGroup";
 const ListingCard = ({ listing }) => {
   return (
     <div className="card-container">
-      <Link to={`/listing/${listing._id}/edit`} state={listing}>
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src={listing.photo} />
-          <Card.Body>
-            <Card.Title>{listing.address}</Card.Title>
-          </Card.Body>
-          <ListGroup className="list-group-flush">
-            <ListGroup.Item>
-              Bed: {listing.bedroom} / Bath: {listing.bathroom}
-            </ListGroup.Item>
-            <ListGroup.Item>Rent: ${listing.rent}</ListGroup.Item>
-          </ListGroup>
-          <Card.Text>
-            Tenants:
-            {listing.tenants.length ?
-              <>
-                {listing.tenants.map((tenant) =>
-                  <li key={tenant._id}>{tenant.name}</li>
-                )}
-              </>
-              :
-              <>
-                <li>No tenants currently</li>
-              </>
-            }
-          </Card.Text>
-          <ListGroup>
-          </ListGroup>
-        </Card>
-      </Link>
-      <Link to="/listings/:id/workRequests" state={listing}>
-        Add Work request
-      </Link>
+      <div>
+        <Link to={`/listing/${listing._id}/edit`} state={listing}>
+          <Card style={{ width: "18rem" }}>
+            <Card.Img variant="top" src={listing.photo} />
+            <Card.Body>
+              <Card.Title 
+                style={{fontSize: "25px", color:"black", fontWeight:"bold"}}
+              >
+                {listing.address}
+              </Card.Title>
+            </Card.Body>
+            <ListGroup className="list-group-flush">
+              <ListGroup.Item style={{fontWeight:"bold", fontSize: "17px"}}>
+                Bedrooms: {listing.bedroom} / Bathrooms: {listing.bathroom}
+              </ListGroup.Item>
+              <ListGroup.Item>Rent: ${listing.rent}</ListGroup.Item>
+            </ListGroup>
+            <Card.Text>
+              Tenants:
+              {listing.tenants.length ?
+                <>
+                  {listing.tenants.map((tenant) =>
+                    <li key={tenant._id}>{tenant.name}</li>
+                  )}
+                </>
+                :
+                <>
+                  <li>No tenants currently</li>
+                </>
+              }
+            </Card.Text>
+            <ListGroup>
+            </ListGroup>
+          </Card>
+        </Link>
+        <Link to="/listings/:id/workRequests" state={listing}>
+          Add Work request
+        </Link>
+      </div>
     </div>
   );
 };
