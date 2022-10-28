41fb7881d47f32c8cd2237632eb76776e4ba03a0
julianboyne <julianboyne11@gmail.com>
2022-10-27 22:21:41 -0400

    nav bar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -20,51 +20,42 @@ const NavBar = ({ user, handleLogout }) => {
             </Link>
           </li>
         </ul>
-        <Menu>
-          <ul className="ham-bar">
-            <li>
-              <Link to="/listings">My Listings</Link>
-            </li>
-            <br />
-            <li>
-              <Link to="/add-listing">Add Listing</Link>
-            </li>
-            <br />
-            <li>
-              <Link to="/add-tenant">Add Tenant</Link>
-            </li>
-            <br />
-            <li>
-              <Link to="/workRequests">Work Requests</Link>
-            </li>
-            <br />
-            <li>
-              <Link to="/profiles">Profiles</Link>
-            </li>
-            <br />
-            <li>
-              <Link to="/change-password">Change Password</Link>
-            </li>
-            <br />
-          </ul>
-      </Menu>
-          </>
-        : 
-          <>
-            <ul className="nav-bar">
+          <Menu>
+            <ul className="ham-bar">
               <li>
-                <Link to="/listings" className="non-login">My Listings</Link>
+                <Link className="ham-bar-item" to="/listings">My Listings</Link>
               </li>
               <li>
-                <Link to="/tenants" className="non-login">Tenants</Link>
+                <Link className="ham-bar-item" to="/add-listing">Add Listing</Link>
               </li>
-              <div className="btn-account">
-              <Link to="/login"><button>Log In</button></Link>
-              <Link to="/signup"><button>Sign Up</button></Link>
-              </div>
+              <li>
+                <Link className="ham-bar-item" to="/add-tenant">Add Tenant</Link>
+              </li>          
+              <li>
+                <Link className="ham-bar-item" to="/workRequests">Work Requests</Link>
+              </li>
+              <li>
+                <Link className="ham-bar-item" to="/change-password">Change Password</Link>
+              </li>
             </ul>
-          </>
-        }
+          </Menu>
+        </>
+      : 
+        <>
+          <ul className="nav-bar">
+            <li>
+              <Link to="/listings" className="non-login">My Listings</Link>
+            </li>
+            <li>
+              <Link to="/tenants" className="non-login">Tenants</Link>
+            </li>
+            <div className="btn-account">
+            <Link to="/login"><button>Log In</button></Link>
+            <Link to="/signup"><button>Sign Up</button></Link>
+            </div>
+          </ul>
+        </>
+      }
     </>
   );
 };
