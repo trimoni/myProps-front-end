322fd62e9f586e23ec7ebfd8829d6d639cee3726
julianboyne <julianboyne11@gmail.com>
2022-10-28 03:58:17 -0400

    add css to listing card

--- src/components/ListingCard/ListingCard.jsx
+++ src/components/ListingCard/ListingCard.jsx
@@ -8,42 +8,44 @@ const ListingCard = ({ listing }) => {
     <div className="card-container">
       <div>
         <Link to={`/listing/${listing._id}/edit`} state={listing}>
-          <Card style={{ width: "18rem" }}>
+          <Card style={{ width: "18rem", background: "#03546d" }}>
             <Card.Img variant="top" src={listing.photo} />
             <Card.Body>
               <Card.Title 
-                style={{fontSize: "25px", color:"black", fontWeight:"bold"}}
+                style={{fontSize: "25px", color:"white", fontWeight:"bold"}}
               >
                 {listing.address}
               </Card.Title>
             </Card.Body>
             <ListGroup className="list-group-flush">
-              <ListGroup.Item style={{fontWeight:"bold", fontSize: "17px"}}>
+              <ListGroup.Item style={{fontWeight:"bold", fontSize: "17px", background: "#03546d", color: "white"}}>
                 Bedrooms: {listing.bedroom} / Bathrooms: {listing.bathroom}
               </ListGroup.Item>
-              <ListGroup.Item style={{fontWeight:"bold"}}>Rent: ${listing.rent}</ListGroup.Item>
+              <ListGroup.Item style={{fontWeight:"bold", background: "#03546d", color:"white"}}>Rent: ${listing.rent}</ListGroup.Item>
             </ListGroup>
-            <Card.Text stylr={{color: "black", fontWeight: "bold"}}>
-              Tenants:
+            <Card.Body stylr={{color:"white", fontWeight: "bold"}}>
+              <h9 style={{fontWeight: "bold", color: "white"}}>Tenants:</h9>
               {listing.tenants.length ?
                 <>
                   {listing.tenants.map((tenant) =>
-                    <li key={tenant._id}>{tenant.name}</li>
+                    <li style={{fontWeight:"bold", fontSize: "15px", color:"white"}} key={tenant._id}>{tenant.name}</li>
                   )}
                 </>
                 :
                 <>
-                  <li>No tenants currently</li>
+                  <p>No tenants currently</p>
                 </>
               }
-            </Card.Text>
+            </Card.Body>
             <ListGroup>
             </ListGroup>
           </Card>
         </Link>
+        <div className="add-work-r-Link">
         <Link to="/listings/:id/workRequests" state={listing}>
           Add Work request
         </Link>
+        </div>
       </div>
     </div>
   );
--- src/pages/EditListing/EditListing.jsx
+++ src/pages/EditListing/EditListing.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link, useLocation, useParams } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 import styles from './EditListing.module.css'
 
 const EditListing = (props) => {
